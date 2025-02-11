a73cffdac52ab5c2164a5c3b7da50d908456fcab
caibucai <caibucai22@gmail.com>
2025-02-11 14:01:34 +0800

    bingo_cls v2

--- service/eval/BingoClsService.py
+++ service/eval/BingoClsService.py
@@ -17,7 +17,7 @@ import numpy as np
 from models.ModelManager_v2 import model_manger
 from tools.Utils import img_load_by_Image, images_convert
 from tools.Logger import get_logger
-
+from ultralytics.engine.results import Boxes, Results
 import logging
 
 logger = get_logger(__file__, log_level=logging.INFO)
@@ -41,3 +41,55 @@ class BingoClsService(IClsService):
             }
             bingo_ret.append(bingo)
         return bingo_ret
+
+
+class BingoClsServiceV2(IClsService):
+    """
+    使用 yolo v8训练的bingo_det 模型 做分类任务 
+    调用逻辑 和 yoloLocate基本一致 进行了简化 没有单独封装 BingoDetModel
+    """
+
+    def __init__(self):
+        self.model = model_manger.get_model("bingo_det")
+
+    def binary_cls(self, cell_images: list) -> list:
+        bingo_ret = []
+        cell_images = images_convert(cell_images, to_type="Image")
+        for cell in cell_images:
+            bingo = {}
+            model_output: Results = self.model.predict(cell)  # auto device
+            boxs = model_output[0].boxes.xyxy.cpu().numpy()
+            conf = model_output[0].boxes.conf.cpu().numpy()
+            if len(boxs) >= 1:
+                bingo = {
+                    "bingo": True,
+                    "conf": conf[0],
+                }
+            else:
+                bingo = {
+                    "bingo": False,
+                    "conf": 0,
+                }
+            bingo_ret.append(bingo)
+        return bingo_ret
+
+
+def test_binary_cls():
+    img_path = (
+        "E:/my-github-repos/01-my_repos/base_output/cache/locate_table_3/cell_5_2.jpg"
+    )
+    model = BingoClsService()
+    bingo_ret = model.binary_cls([img_path])
+    print(bingo_ret)
+
+
+def test_bingo_det():
+    img_path = "E:/my-github-repos/01-my_repos/base_output/cache/locate_table_2/1.jpg"
+    model = BingoDetService()
+    bingo_ret = model.binary_cls([img_path])
+    print(bingo_ret)
+
+
+if __name__ == "__main__":
+    # test_binary_cls()
+    test_bingo_det()
